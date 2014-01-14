7409175bfe11c504fd235f307dc8299fc200caec
Brad Rydzewski <brad.rydzewski@gmail.com>
2014-01-13 17:18:45 -0800

    updated style

--- web/index.dart
+++ web/index.dart
@@ -6,7 +6,7 @@ void main() {
     .onClick.listen(onShorten);
 }
 
-void onShorten(MouseEvent event) {
+void onShorten(MouseEvent event) {  
   // create new xhr
   HttpRequest request = new HttpRequest();
   
@@ -16,35 +16,16 @@ void onShorten(MouseEvent event) {
         (request.status == 200 || request.status == 0)) {
       // data saved OK.
       querySelector("#hash").text = window.location.protocol + "//" + window.location.host + "/" + request.responseText;
+
+      // un-hide
+      querySelector("#hash").classes.remove("hidden");
     }
   });
-
-  // encode the JSON data
+  
+  // get the url field
   InputElement uriInput = querySelector("#url");
   
   // POST the data to the server
   request.open("POST","/?url=${uriInput.value}", async:false); //"/?url=${urlClean}", async: false);
   request.send();
 }
-
-
-//void reverseText(MouseEvent event) {
-//  var text = querySelector("#sample_text_id").text;
-//  var buffer = new StringBuffer();
-//  for (int i = text.length - 1; i >= 0; i--) {
-//    buffer.write(text[i]);
-//  }
-//  querySelector("#sample_text_id").text = buffer.toString();
-//}
-
-
-
-//  var postTo = new Uri.http( window.location.host, "/", { "url" : url });
-//var urlClean = Uri.encodeComponent(url);
-//String jsonData = JSON.encode({ "url" : url });
-//request.send(jsonData);
-
-//void onDataLoaded(String responseText) {
-//  var hash = responseText;
-//  querySelector("#hash").text = hash;
-//}
