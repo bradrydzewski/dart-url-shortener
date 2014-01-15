aa570c7e13d21ac7002a821dc7af05e24dda4473
Matt Norris <matthew.g.norris@gmail.com>
2014-01-14 21:05:51 -0500

    Cleaned up servertests.dart.

--- bin/servertests.dart
+++ bin/servertests.dart
@@ -8,17 +8,6 @@ void main() {
   
   String URL = 'http://www.meetup.com/gdg-silicon-valley/events/159082592/';
   
-  // Will err. For pitch purposes only. 
-  test('HashURL_1', () {
-    expect(server.toHash_1(URL), isNotNull); 
-  });
-      
-  // Will fail. For pitch purposes only. 
-  test('HashURL_2', () {
-    expect(server.toHash_2(URL), isNotNull);
-  });
-  
-  // Will succeed. 
   test('HashURL', () {
     expect(server.toHash(URL), isNotNull);
     expect(server.toHash(URL), '287b6d95');
